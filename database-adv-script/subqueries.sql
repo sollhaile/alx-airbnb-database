2ed49fa3fe566d3d07e356709a1cc4880e868a93
Solomon <102990501+sollhaile@users.noreply.github.com>
2025-05-16 20:52:08 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,5 +1,21 @@
 --Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
 
-SELECT * FROM airbnb_database.review WHERE rating > 4.0
+SELECT *
+FROM property
+WHERE property_id IN (
+    SELECT property_id
+    FROM review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
 
-select u.user_id,u.first_name,b.booking_id from user u inner join booking b ON u.user_id=b.user_id where u.user_id IN (select user_id from booking group by user_id having count(*) > 3);
+
+SELECT u.user_id, u.first_name, b.booking_id
+FROM user u
+INNER JOIN booking b ON u.user_id = b.user_id
+WHERE u.user_id IN (
+    SELECT user_id
+    FROM booking
+    GROUP BY user_id
+    HAVING COUNT(*) > 3
+);
