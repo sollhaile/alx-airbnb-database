f85fd9eb040661a7279af0e7d611c063235f4b7c
Solomon <102990501+sollhaile@users.noreply.github.com>
2025-05-19 10:49:47 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,4 +1,4 @@
-select u.user_id,u.first_name
+SELECT u.user_id,u.first_name
 COUNT(b.booking_id) as total_bookings
-from user u INNER JOIN booking b ON u,user_id=b.user_id
+FROM USER u INNER JOIN booking b ON u,user_id=b.user_id
 GROUP BY u.user_id,u.first_name
