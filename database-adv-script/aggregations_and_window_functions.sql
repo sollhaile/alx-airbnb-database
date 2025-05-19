b2c44502292187fb8a99e6c1810a469991521133
Solomon <102990501+sollhaile@users.noreply.github.com>
2025-05-19 10:51:35 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,4 +1,11 @@
-SELECT u.user_id,u.first_name
-COUNT(b.booking_id) as total_bookings
-FROM USER u INNER JOIN booking b ON u,user_id=b.user_id
-GROUP BY u.user_id,u.first_name
+SELECT 
+  p.property_id,
+  p.name AS property_name,
+  COUNT(b.booking_id) AS total_bookings,
+  RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+  property p
+LEFT JOIN 
+  booking b ON p.property_id = b.property_id
+GROUP BY 
+  p.property_id, p.name;
